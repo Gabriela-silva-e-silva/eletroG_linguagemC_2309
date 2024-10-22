5e0b98e90f11b2fe38bd93cd568b46a00a9450be
Gabriela-silva-e-silva <ggabrielasilvaa1@gmail.com>
2024-10-22 10:35:39 -0300

    Add files via upload

---
+++ 21 - 10mainc_operacao.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+char operacao;
+
+printf("a sua operacao e: \n 1 == saldo; \n 2 == extrato; \n3 == saque;\n 4 == sair; ");
+scanf("%c",&operacao);
+
+
+if (operacao == '1'){
+    printf("a sua operacao e: saldo");
+}else if (operacao == '2'){
+    printf("a sua operacao e: extrato");
+}else if (operacao == '3'){
+    printf("a sua operacao e: saque");
+} else if (operacao == '4'){
+    printf("a sua operacao e: sair");
+}else{
+    printf("a sua operacao invalida");
+}
+}
