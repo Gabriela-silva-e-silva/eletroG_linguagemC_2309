b9ed4d504f5a6b76249819ed2501b4a0b1ec52c2
Gabriela-silva-e-silva <ggabrielasilvaa1@gmail.com>
2024-11-18 08:37:31 -0300

    Add files via upload

---
+++ 18.11mainc_numeros.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ printf("%c",71);
+ printf("%c",65);
+ printf("%c",66);
+ printf("%c",82);
+ printf("%c",73);  
+ printf("%c",69);
+ printf("%c",76);
+ printf("%c",65);
+}
